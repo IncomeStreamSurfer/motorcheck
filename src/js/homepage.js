2f89a9875c3f5f9ae99cf6ae4050ea8d4de28be3
IncomeStreamSurfer <IncomeStreamSurfer@users.noreply.github.com>
2025-07-05 21:24:19 +0100

    Fix hexagon animations to scale from exact center point
    - Adjusted SVG viewBox and positioning for true centering
    - Changed starting scale from 0.3 to 0 for cleaner appearance
    - Set transform-origin to exact hexagon center (52px, 30px)
    - Icons now also scale from 0 to prevent directional movement
    - Ripples start from scale(0) for consistent behavior
    - All elements now expand/contract from their geometric center

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -99,17 +99,19 @@ function initAnimatedHexagons() {
 
         // Create SVG hexagon with centered transform origin
         hexagon.innerHTML = `
-            <svg width="78" height="60" viewBox="0 0 78 60" xmlns="http://www.w3.org/2000/svg" style="position: absolute; left: 0; top: 0;">
+            <svg width="104" height="60" viewBox="0 0 104 60" xmlns="http://www.w3.org/2000/svg" style="position: absolute; left: -13px; top: 0;">
                 <path d="M26 15 L52 0 L78 15 L78 45 L52 60 L26 45 Z" 
-                      fill="${dataPoint.color}" class="hex-fill"/>
+                      fill="${dataPoint.color}" class="hex-fill"
+                      transform-origin="52 30"/>
             </svg>
             <div class="hex-icon">
                 <i data-lucide="${dataPoint.icon}"></i>
             </div>
             <div class="hex-ripple" style="color: ${dataPoint.color};">
-                <svg width="200" height="200" viewBox="-61 -70 200 200" xmlns="http://www.w3.org/2000/svg" style="position: absolute; left: -61px; top: -70px;">
+                <svg width="200" height="200" viewBox="-74 -70 200 200" xmlns="http://www.w3.org/2000/svg" style="position: absolute; left: -74px; top: -70px;">
                     <path class="hex-ripple-path" 
-                          d="M26 15 L52 0 L78 15 L78 45 L52 60 L26 45 Z"/>
+                          d="M26 15 L52 0 L78 15 L78 45 L52 60 L26 45 Z"
+                          transform-origin="52 30"/>
                 </svg>
             </div>
         `;
